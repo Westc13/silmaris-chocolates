2ceacb5b5abd59776c6a899efc17c4be69e9e55b
ChristineWest <christinewwwest@gmail.com>
2022-09-13 11:57:11 -0400

    set up pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+// import { useState, useEffect } from "react";
+// import "./App.scss";
+// import firebase from "./firebase";
+// import { getDatabase, ref, get, set } from "firebase/database";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
+import Contact from "./pages/Contact";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 }
 
 export default App;
