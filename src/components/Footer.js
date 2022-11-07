403ef5f9e249ba32c23118c316c898e7e7dd2070
ChristineWest <christinewwwest@gmail.com>
2022-11-07 15:39:38 -0500

    added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      <div className="wrapper">
+        <h2>Silmarils Chocolates</h2>
+      </div>
+      <p>Made from all natural ingredients and by hand!</p>
+    </footer>
+  );
+};
+export default Footer;
