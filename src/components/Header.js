1cd071729216dbe8ac5667f6c31da0e57c30bdd6
ChristineWest <christinewwwest@gmail.com>
2022-11-07 15:34:34 -0500

    added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="Header">
+      <div className="wrapper">
+        <h2>Silmarils Chocolates</h2>
+        <p>Vita Dulcius Est Scelerisque.</p>
+        <a href="#collection" className="catelogBtn">
+          Shop Now
+        </a>
+      </div>
+    </header>
+  );
+};
