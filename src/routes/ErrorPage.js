4ae247db7f54c0ebd3896925fb4b400b82d6a0fa
ChristineWest <christinewwwest@gmail.com>
2022-11-08 18:16:52 -0500

    fixed typo in ErrorPage route

--- src/routes/ErrorPage.js
+++ src/routes/ErrorPage.js
@@ -1,12 +1,11 @@
-const ErrorPage = (){
-    return (
-        <section className='ErrorPage'>
-            <div className='wrapper'>
-                <h2>404 Error</h2>
-                <p>Looks like the page you are looking for does not exist.</p>
-                <Link to='/'>Back
-                </Link>
-            </div>
-        </section>
-    )
-}
+const ErrorPage = () => {
+  return (
+    <section className="ErrorPage">
+      <div className="wrapper">
+        <h2>404 Error</h2>
+        <p>Looks like the page you are looking for does not exist.</p>
+        <Link to="/">Back</Link>
+      </div>
+    </section>
+  );
+};
